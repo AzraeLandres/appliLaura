fc026f43fa53432317d51b5a1db09f22e7271f0c
AzraeLandres <azraellandres@gmail.com>
2024-02-24 16:52:28 +0100

    Add routes for new components

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { MyMoviesComponent } from './my-movies/my-movies.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { SearchComponent } from './search/search.component';
+import { ToSeeComponent } from './to-see/to-see.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'to-see', component: ToSeeComponent },
+  { path: 'my-movies', component: MyMoviesComponent },
+  { path: 'add-movie', component: AddMovieComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '', redirectTo: '/to-see', pathMatch: 'full' },
+  { path: '**', redirectTo: '/to-see' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
