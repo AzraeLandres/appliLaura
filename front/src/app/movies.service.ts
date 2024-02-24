94a6ff0d6ef05ef2c3367c1dc01497c74b2814a0
AzraeLandres <azraellandres@gmail.com>
2024-02-24 16:44:43 +0100

    Add MenuComponent and update index.html and service and favicon

---
+++ front/src/app/movies.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  private url = 'http://localhost:8080/movies';
+  constructor(private http: HttpClient) {}
+
+  getMovies(): Observable<any> {
+    return this.http.get(this.url + '/all');
+  }
+
+  getMoviesbyGenreId(id: number): Observable<any> {
+    return this.http.get(this.url + '/genre/' + id);
+  }
+}
