f2a48eba8a024af0bc7c9797d82317879941f47d
AzraeLandres <azraellandres@gmail.com>
2024-02-24 13:50:23 +0100

    Add movie and get all movies endpoints to MovieController

--- back/src/main/java/com/laura/movies/Controller/MovieController.java
+++ back/src/main/java/com/laura/movies/Controller/MovieController.java
@@ -1,11 +1,16 @@
 package com.laura.movies.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+import com.laura.movies.Entity.MovieEntity;
 import com.laura.movies.Service.MovieService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/movies")
@@ -13,4 +18,15 @@ public class MovieController {
 
     @Autowired
     private MovieService movieService;
+
+    @PostMapping("/add")
+    public void addMovie(@RequestBody MovieEntity movie) {
+        movieService.save(movie);
+    }
+
+    @GetMapping("/all")
+    public List<MovieEntity> getAllMovies() {
+        return movieService.findAll();
+    }
+
 }
