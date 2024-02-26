bc3d68277f01553468e91e7216625c13d4bc1849
AzraeLandres <azraellandres@gmail.com>
2024-02-26 20:46:08 +0100

    add model

---
+++ front/src/app/models/movie.model.ts
@@ -0,0 +1,42 @@
+export class Movie {
+  id: number;
+  date: Date;
+  vo: string;
+  vf: string;
+  year: number;
+  duration: number;
+  rating: number;
+  tag: string;
+  review: string;
+  seen: boolean;
+  id_genre: number;
+  id_who: number;
+
+  constructor(
+    id: number,
+    date: Date,
+    vo: string,
+    vf: string,
+    year: number,
+    duration: number,
+    rating: number,
+    tag: string,
+    review: string,
+    seen: boolean,
+    id_genre: number,
+    id_who: number
+  ) {
+    this.id = id;
+    this.date = date;
+    this.vo = vo;
+    this.vf = vf;
+    this.year = year;
+    this.duration = duration;
+    this.rating = rating;
+    this.tag = tag;
+    this.review = review;
+    this.seen = seen;
+    this.id_genre = id_genre;
+    this.id_who = id_who;
+  }
+}
